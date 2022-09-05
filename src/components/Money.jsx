eda391eb8d2246bfdf5111ab77348d85e82fe778
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-09-05 15:41:58 +0200

    third commit

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -3,6 +3,14 @@ import { useState } from 'react'
 const Money = () => {
     const [stand, standAdd] = useState(0)
     const [stand1, standAdd1] = useState(0)
+    /* const dispo = () => {
+        if (stand >= 0) {
+            window.prompt('Sie dürfen nicht ins Dispo')
+        }
+        else {
+
+        }
+}*/
 
 
     return (
@@ -13,7 +21,7 @@ const Money = () => {
             <input value={stand} onChange={(e) => { standAdd(e.target.value) }} type="number" placeholder='Bitte Betrag in € eingeben' />
             <button onClick={(e) => { standAdd1((prev) => prev + Number(stand)); standAdd(0) }}>Einzahlen
             </button>
-            <button onClick={(e) => { standAdd1((prev) => prev - Number(stand)); standAdd(0) }}>Auszahlen</button>
+            <button onClick={(e) => { stand1 - stand >= 0 ? standAdd1((prev) => prev - Number(stand)) : window.prompt('Sie dürfen nicht ins Dispo'); standAdd(0); }}>Auszahlen</button>
         </div>
 
     )
