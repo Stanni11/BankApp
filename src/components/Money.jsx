aaf87ad40fa26e1d4818ed2e9601cd8e42f47841
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-09-05 15:48:24 +0200

    4 commit

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -1,27 +1,19 @@
 import { useState } from 'react'
 
 const Money = () => {
-    const [stand, standAdd] = useState(0)
-    const [stand1, standAdd1] = useState(0)
-    /* const dispo = () => {
-        if (stand >= 0) {
-            window.prompt('Sie dürfen nicht ins Dispo')
-        }
-        else {
-
-        }
-}*/
+    const [userInput, userInputGet] = useState(0)
+    const [result, resultGet] = useState(0)
 
 
     return (
         <div className='banana'>
             <h1 className="header">Banana Bank</h1>
             <div className="kontostand">Ihr Kontostand</div>
-            <p>{stand1}</p>
-            <input value={stand} onChange={(e) => { standAdd(e.target.value) }} type="number" placeholder='Bitte Betrag in € eingeben' />
-            <button onClick={(e) => { standAdd1((prev) => prev + Number(stand)); standAdd(0) }}>Einzahlen
+            <p>{result}</p>
+            <input value={userInput} onChange={(e) => { userInputGet(e.target.value) }} type="number" placeholder='Bitte Betrag in € eingeben' />
+            <button onClick={(e) => { resultGet((prev) => prev + Number(userInput)); userInputGet(0) }}>Einzahlen
             </button>
-            <button onClick={(e) => { stand1 - stand >= 0 ? standAdd1((prev) => prev - Number(stand)) : window.prompt('Sie dürfen nicht ins Dispo'); standAdd(0); }}>Auszahlen</button>
+            <button onClick={(e) => { result - userInput >= 0 ? resultGet((prev) => prev - Number(userInput)) : window.prompt('Sie dürfen nicht ins Dispo'); userInputGet(0); }}>Auszahlen</button>
         </div>
 
     )
