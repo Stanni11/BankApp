e917baa8d104c832ef2a8f63833ef835572816bd
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-09-05 15:13:40 +0200

    second commit

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -3,19 +3,19 @@ import { useState } from 'react'
 const Money = () => {
     const [stand, standAdd] = useState(0)
     const [stand1, standAdd1] = useState(0)
-    /* const reset = () => {
-        return input = ''
-    } */
+
 
     return (
-        <>
+        <div className='banana'>
+            <h1 className="header">Banana Bank</h1>
+            <div className="kontostand">Ihr Kontostand</div>
             <p>{stand1}</p>
-            <input value={stand} onChange={(e) => { standAdd(e.target.value) }} type="number" />
+            <input value={stand} onChange={(e) => { standAdd(e.target.value) }} type="number" placeholder='Bitte Betrag in € eingeben' />
             <button onClick={(e) => { standAdd1((prev) => prev + Number(stand)); standAdd(0) }}>Einzahlen
             </button>
             <button onClick={(e) => { standAdd1((prev) => prev - Number(stand)); standAdd(0) }}>Auszahlen</button>
+        </div>
 
-        </>
     )
 }
 
